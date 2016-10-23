ad34142e2f01d5bae5115f17c78cf1be0663032f
Timothy Gu <timothygu99@gmail.com>
2016-10-23 14:45:20 -0700

    Less barebones design
    Or actually have a design

--- generate-web.js
+++ generate-web.js
@@ -3,13 +3,26 @@ const logo = document.querySelector('#logo');
 const form = document.querySelector('#values');
 const ctx = canvas.getContext('2d');
 
+function svgToImage(svg) {
+  const DOMURL = window.URL || window.webkitURL || window;
+
+  return new Promise(fulfill => {
+    const img = new Image();
+    const blob = new Blob([svg], {type: 'image/svg+xml'});
+    const url = DOMURL.createObjectURL(blob);
+    img.onload = () => {
+      fulfill(img);
+      DOMURL.revokeObjectURL(url);
+    };
+    img.src = url;
+  });
+}
+
 function generate() {
-  const values = [...form.children].map(i => i.value);
+  const values = [...form.querySelectorAll('input')].map(i => i.value);
   const [name, date, headingColor, backgroundColor, stop1, stop2] = values;
 
   ctx.textBaseline = 'top';
-
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   ctx.fillStyle = backgroundColor;
   ctx.fillRect(0, 0, 800, 200);
@@ -22,25 +35,28 @@ function generate() {
   ctx.fillRect(0, 170, 800, 30);
 
   let coloredLogo = logo.innerHTML.replace(/\${COLOR}/g, headingColor);
-  let logoSource = 'data:image/svg+xml;base64,' + btoa(coloredLogo);
-  let logoImg = new Image();
-  logoImg.src = logoSource;
-  ctx.drawImage(logoImg, 55, 60, logoImg.width * 0.6, logoImg.height * 0.6);
+  svgToImage(coloredLogo).then(logoImg => {
+    ctx.drawImage(logoImg, 55, 60, logoImg.width * 0.6, logoImg.height * 0.6);
 
-  ctx.font = '700 20pt Lato';
-  ctx.fillStyle = stop2;
-  ctx.fillText(date, 440, 50);
+    ctx.font = '700 20pt Lato';
+    ctx.fillStyle = stop2;
+    ctx.fillText(date, 440, 50);
 
-  ctx.font = '700 30pt Lato';
-  ctx.fillStyle = headingColor;
-  ctx.fillText(name, 440, 80);
+    ctx.font = '700 30pt Lato';
+    ctx.fillStyle = headingColor;
+    ctx.fillText(name, 440, 80);
+  });
 }
 
 form.addEventListener('input', generate);
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  generate();
+});
 
 WebFont.load({
   google: {
-    families: ['Lato']
+    families: ['Lato', 'Lato:bold']
   },
   active: () => {
     generate();
