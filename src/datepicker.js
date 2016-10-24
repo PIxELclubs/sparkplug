be1e0a79557c4b633aac1cc932793e5a7f92f1ed
Timothy Gu <timothygu99@gmail.com>
2016-10-23 17:16:59 -0700

    Better date picker

---
+++ src/datepicker.js
@@ -0,0 +1,14 @@
+[...document.querySelectorAll('.datepicker')].forEach(el => {
+  const $el = $(el);
+  const form = $el.parents('form')[0];
+  const picker = $el.pickadate({
+    select: new Date(),
+    format: 'mmmm d, yyyy',
+    onSet: () => {
+      // better than nothing
+      form.dispatchEvent(new Event('input'));
+    }
+  }).pickadate('picker');
+
+  picker.set('select', new Date());
+});
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import WebFont from 'webfontloader';
-import moment from 'moment';
+import './datepicker.js';
 import generate from './generator';
 
 export {generate};
 export {logo, form, canvas, ctx} from './generator';
-
-document.querySelector('#date').setAttribute('value', moment().format('MMMM D, YYYY'));
 
 WebFont.load({
   google: {
