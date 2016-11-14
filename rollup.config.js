50e47a9dcdf70abdade6225fe02f97c1ee551d3a
Timothy Gu <timothygu99@gmail.com>
2016-11-14 02:01:02 -0800

    Minify

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,20 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import replace from 'rollup-plugin-replace';
 
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+const BUNDLE = 'bundle.js';
+
+function babelBundle(...args) {
+  const upstream = babel(...args);
+  return {
+    name: 'babel-bundle',
+    transformBundle(code) {
+      return upstream.transform(code, BUNDLE);
+    }
+  };
+}
+
 export default {
   entry: 'src/index.browser.js',
   format: 'iife',
@@ -22,14 +36,24 @@ export default {
     babel({
       exclude: 'node_modules/**',
       presets: [
-        'es2015-rollup',
+        ['es2015', {modules: false}],
         'react',
         'stage-2'
+      ],
+      plugins: [
+        'external-helpers'
       ]
     }),
     replace({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+      'process.env.NODE_ENV': JSON.stringify(NODE_ENV)
     })
-  ],
+  ].concat(NODE_ENV === 'production' ? [
+    babelBundle({
+      presets: [
+        'babili'
+      ],
+      comments: false
+    })
+  ] : []),
   dest: 'bundle.js'
 };
